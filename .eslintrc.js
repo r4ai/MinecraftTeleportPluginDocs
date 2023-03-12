9adb26a4836fdef7608ad238c652bc3f57dd101e
rai <mj82xx@gmail.com>
2023-03-12 18:45:13 +0900

    chore: eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'unused-imports'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+    'react/function-component-definition': [
+      //アロー関数以外受け付けない設定
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'import/extensions': [
+      //importのときに以下の拡張子を記述しなくてもエラーにしない
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      //jsx形式のファイル拡張子をjsxもしくはtsxに限定
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off', //import React from 'react'が無くてもエラーを無くす
+    'react/prop-types': 'off', //TypeScriptでチェックしているから不要。offにする
+    'no-void': [
+      //void演算子の許可
+      'error',
+      {
+        allowAsStatement: true,
+      },
+    ],
+  },
+};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Minecraft Teleport Plugin</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/r4ai/MinecraftTeleportPlugin',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/r4ai/MinecraftTeleportPluginDocs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Minecraft Teleport Plugin',
   },
-}
+};
 
-export default config
+export default config;
