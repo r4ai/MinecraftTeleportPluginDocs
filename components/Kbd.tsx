30062267f356b90caa6a027dda060a37f2d43838
rai <mj82xx@gmail.com>
2023-03-12 21:27:55 +0900

    fix: kbdコンポーネントをlight modeへ対応

--- components/Kbd.tsx
+++ components/Kbd.tsx
@@ -1,10 +1,16 @@
 import { FC, ReactNode } from 'react';
+import { useTheme } from 'nextra-theme-docs';
 
 interface KbdProps {
-  size: 'xs' | 'sm' | 'md' | 'lg';
   children: ReactNode;
 }
 
-export const Kbd: FC<KbdProps> = ({ size = 'sm', children }) => {
-  return <kbd className={`kbd kbd-${size}`}>{children}</kbd>;
+export const Kbd: FC<KbdProps> = ({ children }) => {
+  const { theme } = useTheme();
+
+  return (
+    <kbd data-theme={theme} className={`kbd kbd-sm ${theme === 'dark' ? 'text-gray-200' : 'text-gray-800'}`}>
+      {children}
+    </kbd>
+  );
 };
