7f12ff0db69ad753bc0f1677773658bb5b8146da
rai <mj82xx@gmail.com>
2023-03-12 22:49:19 +0900

    feat: add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://minecraft-teleport-plugin-docs.vercel.app' + (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return {
+      title: frontMatter.title || 'MinecraftTeleportPlugin',
+      description: frontMatter.description || 'A Minecraft plugin',
+      url,
+      openGraph: {
+        url,
+        title: frontMatter.title || 'MinecraftTeleportPlugin',
+        description: frontMatter.description || 'A Minecraft plugin',
+      },
+    };
+  },
+  head: () => {
+    return (
+      <>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  },
   logo: <span>Minecraft Teleport Plugin</span>,
   project: {
     link: 'https://github.com/r4ai/MinecraftTeleportPlugin',
