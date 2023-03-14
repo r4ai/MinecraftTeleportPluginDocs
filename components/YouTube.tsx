531859f598a1e9ad5fd73a0ea1e95fd81c9d2ce8
rai <mj82xx@gmail.com>
2023-03-14 17:43:28 +0900

    feat: YouTube Embedded Player

---
+++ components/YouTube.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+
+interface YouTubeProps {
+  id: string;
+  title: string;
+  muted: boolean;
+  autoplay: boolean;
+}
+
+export const YouTube: FC<YouTubeProps> = ({ id, title, muted = true, autoplay = true }) => {
+  const autoPlayParam = autoplay ? 'autoplay=1' : 'autoplay=0';
+  const params = [
+    autoPlayParam,
+    'controls=1',
+    'loop=1',
+    'modestbranding=1',
+    'playsinline=1',
+    'showinfo=0',
+    'rel=0',
+  ].join('&');
+  console.log(params);
+  return <LiteYouTubeEmbed id={id} title={title} muted={muted} params={params} />;
+};
